ff8911945053bca714e4d23f6c89b288ff8f5fd9
Alex Gabriel <alexg@criticalmass.com>
2014-12-09 16:36:25 -0500

    Remove requirejs for now. Add entities for modules.

---
+++ code/assets/js/entities/contact.js
@@ -0,0 +1,33 @@
+ContactManager.module("Entities", function(Entities, ContactManager,
+  Backbone, Marionette, $, _) {
+
+  Entities.Contact = Backbone.Model.extend({});
+
+  Entities.ContactCollection = Backbone.Collection.extend({
+    model: Entities.Contact,
+    comparator: "firstName"
+  });
+
+  var contacts;
+
+  var initializeContacts = function() {
+    contacts = new Entities.ContactCollection([
+      { id: 1, firstName: "Alice", lastName: "Artes", phoneNumber: "555-0184" },
+      { id: 2, firstName: "Bob", lastName: "Mar", phoneNumber: "222-0184" },
+      { id: 3, firstName: "Jimmy", lastName: "Bag", phoneNumber: "333-0184" }
+    ]);
+  };
+
+  var API = {
+    getContactEntities: function() {
+      if(contacts === undefined) {
+        initializeContacts();
+      }
+      return contacts;
+    }
+  };
+
+  ContactManager.reqres.setHandler("contact:entities", function() {
+    return API.getContactEntities();
+  });
+});
